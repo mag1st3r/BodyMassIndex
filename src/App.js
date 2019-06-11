6e93e139173eac8f82696ab1778760eaef9294ed
mag1st3r_Note <mmisha199324@gmail.com>
2019-06-11 10:43:38 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+
+const inputNames = {
+  height: {
+    title: "Height",
+    min: "90",
+    max: "245",
+    od:  "cm"
+  },
+  weight: {
+    title:"Weight",
+    min: "35",
+    max: "200",
+    od: "kg"
+  }
+};
+
+class ParametersInput extends React.Component {
+  handleChange = (e) => {
+    const bmi = this.props.bmi;
+
+    this.props.onHeightChange ? this.props.onHeightChange(e.target.value, bmi):
+    this.props.onWeightChange(e.target.value, bmi);
+  };
+
+  render() {
+    const {title, min, max, od} = this.props.param;
+    const {value} = this.props;
+
+    return (
+        <div>
+            <label
+            style={ {fontSize: '18px'}}>
+                {title}:
+                {min}
+                <input
+                    type='range'
+                    name={title}
+                    min={min}
+                    max={max}
+                    step='1'
+                    defaultValue={value}
+                    onChange={this.handleChange}
+                />
+                  {max} <span
+                    style={ {fontWeight: 'bold'}}>
+                    {value}
+                    </span> <span> {od} </span>
+            </label>
+        </div>
+    );
+  }
+ }
+
+ function calculateBmi(h, w) {
+     return  (w / ((h / 100) ** 2)).toFixed(1);
+ }
+
+function resultText(bmi) {
+    return bmi <= 18.5 ? "Underweight"   :
+           bmi <= 24.9 ? "Normal Weight" :
+           bmi <= 29.9 ? "Overweight"    :
+                         "Obese"
+}
+
+function ResultBmi(props) {
+        return (
+          <div>
+            BMI:{props.bmi} {resultText(props.bmi)}
+          </div>
+    );
+}
+
+
+class Calculator extends React.Component {
+  state = {
+    bmi: null,
+    height: 90,
+    weight: 45
+  };
+
+
+  onHeightChange = (height, bmi) => {
+    this.setState({
+        height: height,
+        bmi: bmi
+    });
+
+  };
+
+  onWeightChange = (weight, bmi) =>{
+    this.setState({
+        weight: weight,
+        bmi: bmi
+    });
+
+  };
+
+  render() {
+    const sHeight = this.state.height;
+    const sWeight = this.state.weight;
+    const resultBmi = calculateBmi(sHeight, sWeight);
+    return (
+        <div>
+             <ParametersInput
+                 param={inputNames.height}
+                 value={sHeight}
+                 onHeightChange={this.onHeightChange}
+                 bmi={resultBmi}
+             />
+              <ParametersInput
+                  param={inputNames.weight}
+                  value={sWeight}
+                  onWeightChange={this.onWeightChange}
+                  bmi={resultBmi}
+              />
+              <ResultBmi
+                  bmi={resultBmi}
+              />
+
+        </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Calculator/>
   );
 }
 
